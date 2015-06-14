fac90ed287fca2bd3b5833b96de1eb181d88af9c
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-06-14 18:40:02 +0900

    Revert "fix false-positive identification of EUC-TW text (and spec)"
    This reverts commit b475909c19a14df83426e4f5699117501dbb43e8.
    This commit occured regression for Shift_JIS detection.

--- lib/rchardet/universaldetector.rb
+++ lib/rchardet/universaldetector.rb
@@ -29,7 +29,6 @@
 ######################### END LICENSE BLOCK #########################
 
 module CharDet
-  MINIMUM_DATA_THRESHOLD = 4
   MINIMUM_THRESHOLD = 0.20
   EPureAscii = 0
   EEscAscii = 1
